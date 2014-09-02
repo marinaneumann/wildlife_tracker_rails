229ade17b2d4530c7d72cde7a658fc58d2dbd5c8
Marina Neumann <marinaneumann2468@yahoo.com>
2014-09-02 11:41:08 -0700

    Add ability to create and show a new animal.

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,28 @@
+class AnimalsController < ApplicationController
+  def index
+    @animals = Animal.all
+    render('animals/index.html.erb')
+  end
+
+  def show
+    @animal = Animal.find(params[:id])
+    render('animals/show.html.erb')
+  end
+
+  def new
+    @animal = Animal.new
+    render('animals/new.html.erb')
+  end
+
+  def create
+    @animal = Animal.new(:name => params[:name])
+
+    if @animal.save
+      render('animals/success.html.erb')
+    else
+      render('animals/new.html.erb')
+    end
+
+  end
+
+end
