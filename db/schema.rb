032c013ef33deb7713b4b2aad955279bf7a91c8c
Marina Neumann <marinaneumann2468@yahoo.com>
2014-09-02 15:01:22 -0700

    Validate the presence of name for the region class and introduce updated schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140902170050) do
+ActiveRecord::Schema.define(version: 20140902215315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "animals", force: true do |t|
+    t.string "name"
+  end
+
+  create_table "regions", force: true do |t|
     t.string "name"
   end
 
