27b0df1da6ddda5e6f356b5abf29c63276d92397
Marina Neumann <marinaneumann2468@yahoo.com>
2014-09-10 13:42:27 -0700

    Create association that an animal has many sightings and that an animal has many regions through sightings.

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,7 @@
 class Animal < ActiveRecord::Base
   validates :name, :presence => true
   validates_uniqueness_of :name
+
+  has_many :sightings, :dependent => :destroy
+  has_many :regions, through: :sightings
 end
--- spec/animal_spec.rb
+++ spec/animal_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 describe Animal do
   it { should validate_presence_of :name }
   it { should validate_uniqueness_of :name}
+
+  it { should have_many :sightings}
+  it { should have_many :regions}
 end
