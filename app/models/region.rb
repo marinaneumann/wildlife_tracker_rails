5d50fa1f599629fcc9bc3b7987a7ba31c84169d3
Marina Neumann <marinaneumann2468@yahoo.com>
2014-09-10 13:43:37 -0700

    Create association that a region has many sightings and has many animals through sightings.

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,4 +1,7 @@
 class Region < ActiveRecord::Base
   validates :name, :presence => true
   validates_uniqueness_of :name
+
+  has_many :sightings, :dependent => :destroy
+  has_many :animals, through: :sightings
 end
