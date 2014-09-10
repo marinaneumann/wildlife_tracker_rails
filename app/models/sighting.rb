ab070a7caea4e23c2647412cf5d619dc82c6a6ee
Marina Neumann <marinaneumann2468@yahoo.com>
2014-09-10 13:02:29 -0700

    Associate that a sighting belongs to both an animal and a region.

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -2,4 +2,6 @@ class Sighting < ActiveRecord::Base
   validates :animal_id, :presence => true
   validates :region_id, :presence => true
 
+  belongs_to :animal
+  belongs_to :region
 end
--- spec/sighting_spec.rb
+++ spec/sighting_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 describe Sighting do
   it{ should validate_presence_of :animal_id}
   it{ should validate_presence_of :region_id}
+
+  it{ should belong_to :region}
+  it{ should belong_to :animal}
 end
